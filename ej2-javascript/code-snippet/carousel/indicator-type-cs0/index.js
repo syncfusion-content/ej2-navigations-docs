56e345149a78c612f0ac07c2ebd0adbd1bf4050f
RamRajuE <107187629+RamRajuE@users.noreply.github.com>
2023-06-22 10:53:51 +0530

    bug(ES-831739): content drafted for the indicator type ug doc

--- ej2-javascript/code-snippet/carousel/indicator-type-cs0/index.js
+++ ej2-javascript/code-snippet/carousel/indicator-type-cs0/index.js
@@ -1,15 +1,12 @@
-window.getContent = function (index) {
-    var slides = ["Slide 1", "Slide 2", "Slide 3", "Slide 4", "Slide 5"];
-    return slides[index];
-};
+
 var carouselObj = new ej.navigations.Carousel({
     items: [
-        { template: '<div class="slide-content">Slide 1</div>' },
-        { template: '<div class="slide-content">Slide 2</div>' },
-        { template: '<div class="slide-content">Slide 3</div>' },
-        { template: '<div class="slide-content">Slide 4</div>' },
-        { template: '<div class="slide-content">Slide 5</div>' },
-    ],
+        { template: '<figure class="img-container"><img src="https://ej2.syncfusion.com/products/images/carousel/cardinal.png" alt="cardinal" style="height:100%;width:100%;" /><figcaption class="img-caption">Cardinal</figcaption></figure>' },
+        { template: '<figure class="img-container"><img src="https://ej2.syncfusion.com/products/images/carousel/hunei.png" alt="kingfisher" style="height:100%;width:100%;" /><figcaption class="img-caption">Kingfisher</figcaption></figure>' },
+        { template: '<figure class="img-container"><img src="https://ej2.syncfusion.com/products/images/carousel/costa-rica.png" alt="keel-billed-toucan" style="height:100%;width:100%;" /><figcaption class="img-caption">Keel-billed-toucan</figcaption></figure>' },
+        { template: '<figure class="img-container"><img src="https://ej2.syncfusion.com/products/images/carousel/kaohsiung.png" alt="yellow-warbler" style="height:100%;width:100%;" /><figcaption class="img-caption">Yellow-warbler</figcaption></figure>' },
+        { template: '<figure class="img-container"><img src="https://ej2.syncfusion.com/products/images/carousel/bee-eater.png" alt="bee-eater" style="height:100%;width:100%;" /><figcaption class="img-caption">Bee-eater</figcaption></figure>' }
+      ],
     indicatorsType: "Default",
 });
 carouselObj.appendTo('#carousel');
--- ej2-javascript/code-snippet/carousel/indicator-type-cs0/index.ts
+++ ej2-javascript/code-snippet/carousel/indicator-type-cs0/index.ts
@@ -2,21 +2,13 @@
 
 import { Carousel } from "@syncfusion/ej2-navigations";
 
-(window as TemplateFunction).getContent = (index: number) => {
-  const slides: string[] = ["Slide 1", "Slide 2", "Slide 3", "Slide 4", "Slide 5"];
-  return slides[index];
-};
-interface TemplateFunction extends Window {
-  getContent?: Function;
-}
-
 const carouselObj: Carousel = new Carousel({
   items: [
-    { template: '<div class="slide-content">Slide 1</div>' },
-    { template: '<div class="slide-content">Slide 2</div>' },
-    { template: '<div class="slide-content">Slide 3</div>' },
-    { template: '<div class="slide-content">Slide 4</div>' },
-    { template: '<div class="slide-content">Slide 5</div>' },
+    { template: '<figure class="img-container"><img src="https://ej2.syncfusion.com/products/images/carousel/cardinal.png" alt="cardinal" style="height:100%;width:100%;" /><figcaption class="img-caption">Cardinal</figcaption></figure>' },
+    { template: '<figure class="img-container"><img src="https://ej2.syncfusion.com/products/images/carousel/hunei.png" alt="kingfisher" style="height:100%;width:100%;" /><figcaption class="img-caption">Kingfisher</figcaption></figure>' },
+    { template: '<figure class="img-container"><img src="https://ej2.syncfusion.com/products/images/carousel/costa-rica.png" alt="keel-billed-toucan" style="height:100%;width:100%;" /><figcaption class="img-caption">Keel-billed-toucan</figcaption></figure>' },
+    { template: '<figure class="img-container"><img src="https://ej2.syncfusion.com/products/images/carousel/kaohsiung.png" alt="yellow-warbler" style="height:100%;width:100%;" /><figcaption class="img-caption">Yellow-warbler</figcaption></figure>' },
+    { template: '<figure class="img-container"><img src="https://ej2.syncfusion.com/products/images/carousel/bee-eater.png" alt="bee-eater" style="height:100%;width:100%;" /><figcaption class="img-caption">Bee-eater</figcaption></figure>' }
   ],
   indicatorsType: "Default",
 });
